e17f070a6cafea57557797fbcda8553ff8ede5b0
Léopold Goudier <leopold.goudier@gmail.com>
2025-04-16 15:30:40 +0200

    Ajout de GitHub Actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,25 @@
-import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
-
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  // N'ignore pas tous les JS !
+  {
+    ignores: ['node_modules/**', 'dist/**'], // Ignorer uniquement les dossiers classiques
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        node: true
+      }
+    },
+    rules: {
+      'no-console': 'warn',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single']
+    }
+  }
 ]);
